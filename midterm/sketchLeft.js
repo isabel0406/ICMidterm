cdc67a16d3f5032cde8e208386ed7e3437ed02b1
isabel0406 <113388495+isabel0406@users.noreply.github.com>
2022-10-31 15:31:37 -0400

    Update sketchLeft.js

--- midterm/sketchLeft.js
+++ midterm/sketchLeft.js
@@ -1,89 +1,114 @@
-let theBlocks = [];
-let temp;
-let block1,block2,block3,block4,block5,block6,block7,block8,block9,block10;
-//import block's images
+let blockImg;
+let m,n;
+let tileSize = 25;
+let dx = 0;
+let board = [];
+let shape;
+let canMove = true;
+//create array for moving figures
+let current = [];
+//create rules of blocks
+let block = [
+  //shape "O"
+  [2,3,4,5],
+  //shape "J"
+  [3,5,7,6],
+  //shape "L"
+  [2,3,5,7],
+  //shape "T"
+  [3,5,4,7],
+  //shape "S"
+  [3,5,4,6],
+  //shape "Z"
+  [2,4,5,7],
+  //shape "I"
+  [1,3,5,7]
+];
+let timer = 0;
+let delayed = 0.8
 function preload(){
-  block1 = loadImage("blocks/block1.png");
-  block2 = loadImage("blocks/block2.png");
-  block3 = loadImage("blocks/block3.png");
-  block4 = loadImage("blocks/block4.png");
-  block5 = loadImage("blocks/block5.png");
-  block6 = loadImage("blocks/block6.png");
-  block7 = loadImage("blocks/block7.png");
-  block8 = loadImage("blocks/block8.png");
-  block9 = loadImage("blocks/block9.png");
-  block10 = loadImage("blocks/block10.png");
+  blockImg = loadImage("blocks/block3.png");
 }
 function setup(){
-  imageMode(CENTER);
   createCanvas(400,600);
-  // cnv.parent("#canvas_container");
-  // cnv.style("display","block");
-  // cnv.style("margin","auto");
 
-  // temp = new Blocks(width/2,-50);
-
-
+  //set the canvs into m*n grids
+  m = int(height/tileSize);
+  n = int(width/tileSize);
+  for (let i = 0; i < m; i++){
+    board[i] = new Array(m);
+    for(let k = 0;k<n;k++){
+      //add space for no blocks
+      board[i][k] = 0;
+    }
+  }
+  //create figures
+  for(let j = 0; j < 4;j++){
+    current[j] = new Blocks();
+  }
+  //create random shape in block array
+  shape = int(random(7));
 
 }
 function draw(){
   background(220);
-  fill(92, 128, 188);
-  noStroke();
-  rect(0,0,5,height);
-  rect(395,0,5,height);
-  console.log(temp);
 
-  if(frameCount%120 == 0){
-    temp = int(random(1,11));
-    let nb = new Blocks(width/2,-50,temp);
-    theBlocks.push(nb);
+  drawBlocks();
+  check();
+
+
+  for (let h = 0; h <m; h ++){
+    for (let k = 0; k <n; k++){
+      text(board[h][k],k*tileSize,h*tileSize);
+    }
   }
-  for(let i = 0; i<theBlocks.length;i++){
-    theBlocks[i].display();
-    theBlocks[i].move();
+
+  //console.log(frameCount);
+  timer += deltaTime / 1000;
+  if(timer>delay){
+    for(i = 0; i<4;i++){
+        current[i].move();
+    }
+  }
+  for (let i = 0 ;i < 4;i++){
+    if(keyIsDown(65)){
+      dx -= 0.1;
+    }else if (keyIsDown(68)){
+      dx += 0.1;
+    }
+  }
+  for( i = 0; i < 4; i++){
+    //console.log(current[i].x);
+    image(blockImg,current[i].x*tileSize,current[i].y*tileSize,tileSize,tileSize);
   }
 
 }
 
 class Blocks{
-  constructor(x,y,temp){
-
-    this.xPos = x;
-    this.yPos = y;
-    this.xSpeed = 5;
-    this.ySpeed = 10;
-    this.shape = temp;
-
-  }
-  display(){
-    if(this.shape == 1){
-      image(block1,this.xPos,this.yPos,100,100);
-    }else if(this.shape == 2){
-      image(block2,this.xPos,this.yPos,100,100);
-    }else if(this.shape == 3){
-      image(block3,this.xPos,this.yPos,100,100);
-    }else if(this.shape == 4){
-      image(block4,this.xPos,this.yPos,100,100);
-    }else if(this.shape == 5){
-      image(block5,this.xPos,this.yPos,100,100);
-    }else if(this.shape == 6){
-      image(block6,this.xPos,this.yPos,100,100);
-    }
+  constructor(){
+    this.x = 0;
+    this.y = 0;
   }
   move(){
-    // console.log("move");
-    if(keyIsDown(65)){
-      this.xPos -= this.xSpeed;
-    } else if(keyIsDown(68)){
-      this.xPos += this.xSpeed;
-    }
-    if(keyIsDown(83)){
-      this.yPos += this.ySpeed*2;
-    }else if(frameCount%60 == 0){
-      this.yPos += this.ySpeed;
-    }
-    console.log(this.xPos);
+    this.x+= dx;
+    this.y+=1;
+    board[this.y][this.x]=1;
   }
+}
+
+function drawBlocks(){
+
+  //compute the x and y position of each block
+  for(let i = 0; i<4; i++){
+    current[i].x = block[shape][i]%2+n/2;
+    current[i].y = int(block[shape][i]/2)-1;
+    //board[current[i].y][current[i].x] = 1;
+  }
+
+  delay = 0.8;
+}
+
+function check(){
+  // to check if current grid has been placed blockS
 
 }
